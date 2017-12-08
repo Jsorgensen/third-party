55107f97ef88d4e37d2b1428622f35af98e9db4a
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-12-08 14:30:51 -0700

    initial commit

---
+++ app/src/main/java/jsorgensen/third_party_stuff/ThirdPartyStuff.kt
@@ -0,0 +1,12 @@
+package jsorgensen.third_party_stuff
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdPartyStuff : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_party_stuff)
+    }
+}
